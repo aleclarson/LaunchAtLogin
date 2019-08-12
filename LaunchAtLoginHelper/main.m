7b5768ffd78050c9f4bfd03661f16c5c303be7d7
Alec Larson <alec.stanford.larson@gmail.com>
2019-08-12 12:20:54 -0400

    feat: add logs for debugging

--- LaunchAtLoginHelper/main.m
+++ LaunchAtLoginHelper/main.m
@@ -8,11 +8,21 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
 	NSString *helperId = NSBundle.mainBundle.bundleIdentifier;
-	NSString *bundleId = [helperId substringToIndex:helperId.length - @"-LaunchAtLoginHelper".length];
+	NSLog(@"helperId => %@", helperId);
 	
-	if ([NSRunningApplication runningApplicationsWithBundleIdentifier:bundleId].count == 0) {
+	NSString *bundleId = [helperId substringToIndex:helperId.length - @"-LaunchAtLoginHelper".length];
+	NSLog(@"bundleId => %@", bundleId);
+	
+	NSArray *running = [NSRunningApplication runningApplicationsWithBundleIdentifier:bundleId];
+	NSLog(@"isRunning => %@", running.count > 0 ? @"YES" : @"NO");
+	
+	if (running.count == 0) {
 		NSArray<NSString *> *parts = NSBundle.mainBundle.bundlePath.pathComponents;
+		NSLog(@"helperPathComponents => %@", parts);
+		
 		NSString *mainPath = [NSString pathWithComponents:[parts subarrayWithRange:(NSRange){0, parts.count - 4}]];
+		NSLog(@"Launching: %@", mainPath);
+		
 		[NSWorkspace.sharedWorkspace launchApplication:mainPath];
 	}
 	
